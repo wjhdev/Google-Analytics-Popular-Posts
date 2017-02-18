37a7961fe628ea12e114b1009fe5974b000f8228
Will Haynes <haynes24@gmail.com>
2017-02-18 12:49:55 -0600

    This doesn't work
    & things don’t disappear in git

--- analytic-bridge.php
+++ analytic-bridge.php
@@ -13,9 +13,6 @@ if ( ! defined ( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Toggle this to generate fake data.
-define( 'FAKE_API' , FALSE );
-
 /** Table defines */
 global $wpdb;
 define('METRICS_TABLE', $wpdb->prefix . "analyticbridge_metrics");
@@ -322,90 +319,3 @@ Class AnalyticBridgeLog {
 	}
 
 }
-
-
-/**
- * Class to fake Google Analytics requests.
- *
- * This class extends Google_Service_Analytics and fakes requests to the
- * Google API for debugging by generating data based on the global Wordpress
- * object.
- *
- * Needs work.
- *
- * @since v0.1
- */
-class Google_Service_Analytics_Generator extends Google_Service_Analytics {
-
-	public $data_ga;
-
-	/**
-	 * Construct a new Analytics Generator.
-	 *
-	 * data_ga is set to ourselves. We handle the get function
-	 * internally.
-	 *
-	 * @since v0.1
-	 */
-	public function __construct(Google_Client $client) {
-		$this->data_ga = $this;
-	}
-
-	/**
-	 * Overrided Google_Service_Analytics_DataGa_Resource get function.
-	 *
-	 * @return Google_Service_Analytics_GaData Object with proper row values.
-	 */
-	public function get($ids, $startDate, $endDate, $metrics, $optParams = array()) {
-
-		$rows = array();
-		$metrics = explode(",",$metrics);
-
-		$the_query = new WP_Query(  array( 'post_type' => 'post') );
-
-		if ( $the_query->have_posts() ) :
-
-			while ( $the_query->have_posts() ) : $the_query->the_post();
-
-				$r = array();
-				$url = parse_url(get_permalink());
-				$r[0] = $url['path'] . $url['query'];
-
-				foreach($metrics as $m) {
-
-					if( $m == 'ga:sessions' ) {
-						$r[] = 100 + rand(-25,25);
-					} else if( $m == 'ga:pageviews') {
-						$r[] = 130 + rand(-25,25);
-					} else if ( $m == 'ga:exits' ) {
-						$r[] = 30 + rand(-10,10);
-					} else if ( $m == 'ga:bounceRate' ) {
-						$r[] = 60 + rand(-30,40);
-					} else if ( $m == 'ga:avgSessionDuration' ) {
-						$r[] = 231 + rand(-100,100);
-					} else if ( $m = 'ga:avgTimeOnPage' ) {
-						$r[] = 140 + rand(-40,130);
-					} else {
-						$r[] = 0;
-					}
-
-				}
-
-				$rows[] = $r;
-
-			endwhile;
-
-			wp_reset_postdata();
-
-		endif;
-
-		$toRet = new Google_Service_Analytics_GaData();
-		$toRet->rows = $rows;
-
-		return $toRet;
-
-	}
-
-}
-
-include_once( plugin_dir_path( __FILE__ ) . 'classes/AnalyticBridgePopularPosts.php');
